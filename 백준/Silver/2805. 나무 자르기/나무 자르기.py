25575f304f27c3ad7756ce527cb8cb2fd4f2e2c3
TaeMin Kim <100029499+taeminkimdev@users.noreply.github.com>
2024-07-21 01:07:59 +0900

    [Silver II] Title: 나무 자르기, Time: 460 ms, Memory: 224464 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -3,37 +3,30 @@ import sys
 N, M = map(int, sys.stdin.readline().strip().split())
 
 trees = [int(x) for x in sys.stdin.readline().strip().split()]
-total_cache = {}
 
 
 def cutting(height):
-    try:
-        total = total_cache[height]
-    except Exception:
-        total = 0
-        for tree in trees:
-            rest = tree - height
-            if rest > 0:
-                total += rest
-        total_cache[height] = total
+    total = 0
+    for tree in trees:
+        rest = tree - height
+        if rest > 0:
+            total += rest
 
     return total
 
 
 def search(start, end):
-    mid = int((start + end) / 2)
-    if start == mid:
-        return start
+    while start + 1 != end:
+        mid = int((start + end) / 2)
+        total = cutting(mid)
+        if total == M:
+            return mid
+        elif total > M:
+            start = mid
+        elif total < M:
+            end = mid
 
-    total = cutting(mid)
-    if total == M:
-        return mid
-
-    if total >= M:
-        return search(mid, end)
-
-    if total < M:
-        return search(start, mid)
+    return start
 
 
 print(search(0, max(trees)))
