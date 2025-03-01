4b352729120148d586c40d6683218af97a25b5e8
TaeMin Kim <100029499+xoals3094@users.noreply.github.com>
2025-03-02 00:37:23 +0900

    [Gold IV] Title: 문자열 폭발, Time: 188 ms, Memory: 173692 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -3,14 +3,12 @@ import sys
 string = sys.stdin.readline().strip()
 boom = sys.stdin.readline().strip()
 
-hash_check = {boom: True}
-
 stack = []
 for c in string:
     stack.append(c)
     if len(stack) < len(boom):
         continue
-    if stack[-1] == boom[-1] and ''.join(stack[len(stack) - len(boom):]) in hash_check:
+    if stack[-1] == boom[-1] and ''.join(stack[len(stack) - len(boom):]) == boom:
         for _ in range(len(boom)):
             stack.pop()
 
