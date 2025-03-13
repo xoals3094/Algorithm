c0481d378b92e05a63a3788efcf28d467b685927
TaeMin Kim <100029499+xoals3094@users.noreply.github.com>
2025-03-13 16:23:53 +0900

    [Silver II] Title: 랜선 자르기, Time: 96 ms, Memory: 110712 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,27 +1,26 @@
 import sys
-k, n = map(lambda x: int(x), sys.stdin.readline().split(' '))
 
-lan_list = []
-for _ in range(k):
-    lan_list.append(int(sys.stdin.readline()))
+K, N = map(int, sys.stdin.readline().strip().split())
+lan_list = [int(sys.stdin.readline().strip()) for _ in range(K)]
 
+def binary_search():
+    lan = max(lan_list)
+    result = 1
 
-def count_max_cutting_lan(cm: int):
-    lan_count = 0
-    for lan in lan_list:
-        lan_count += lan // cm
-    return lan_count
+    start = 1
+    end = lan
+    while start <= end:
+        center = (start + end) // 2
+        count = 0
+        for l in lan_list:
+            count += l // center
 
+        if count >= N:
+            result = max(result, center)
+            start = center + 1
+        else:
+            end = center - 1
 
-min_lan = 1
-max_lan = max(lan_list)
-while min_lan + 1 < max_lan:
-    mid_lan = (min_lan + max_lan) // 2
-    if count_max_cutting_lan(mid_lan) < n:
-        max_lan = mid_lan
-    else:
-        min_lan = mid_lan
-if count_max_cutting_lan(max_lan) >= n:
-    print(max_lan)
-else:
-    print(min_lan)
+    return result
+
+print(binary_search())
