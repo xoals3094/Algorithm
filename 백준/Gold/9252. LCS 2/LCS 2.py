f9971b67d18d0135e85100f939cc2ce18b26a5bd
TaeMin Kim <100029499+xoals3094@users.noreply.github.com>
2025-03-15 14:31:03 +0900

    [Gold IV] Title: LCS 2, Time: 128 ms, Memory: 118468 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -4,13 +4,31 @@ A = sys.stdin.readline().strip()
 B = sys.stdin.readline().strip()
 
 
-dp = [['' for _ in range(len(A) + 1)] for _ in range(len(B) + 1)]
+dp = [[0 for _ in range(len(A) + 1)] for _ in range(len(B) + 1)]
 for i in range(1, len(B) + 1):
     for j in range(1, len(A) + 1):
         if A[j - 1] == B[i - 1]:
-            dp[i][j] = dp[i - 1][j - 1] + A[j - 1]
+            dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], key=lambda x: len(x))
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-print(len(dp[len(B)][len(A)]))
-print(dp[len(B)][len(A)])
+lcs = []
+i = len(B)
+j = len(A)
+while dp[i][j] != 0:
+    if dp[i][j] == dp[i][j - 1]:
+        j = j - 1
+    elif dp[i][j] == dp[i - 1][j]:
+        i = i - 1
+    else:
+        lcs.append(A[j - 1])
+        i = i - 1
+        j = j - 1
+
+lcs.reverse()
+
+print(len(lcs))
+for x in lcs:
+    print(x, end='')
+
+
