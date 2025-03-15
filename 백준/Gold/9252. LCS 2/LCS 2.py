31c870a28c455bc56006232e759bac87e7f3f98c
TaeMin Kim <100029499+xoals3094@users.noreply.github.com>
2025-03-15 14:05:15 +0900

    [Gold IV] Title: LCS 2, Time: 548 ms, Memory: 475248 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,21 +1,16 @@
 import sys
 
-LEN = 0
-LCS = 1
-
 A = sys.stdin.readline().strip()
 B = sys.stdin.readline().strip()
 
 
-dp = [[[0, ''] for _ in range(len(A) + 1)] for _ in range(len(B) + 1)]
+dp = [['' for _ in range(len(A) + 1)] for _ in range(len(B) + 1)]
 for i in range(1, len(B) + 1):
     for j in range(1, len(A) + 1):
         if A[j - 1] == B[i - 1]:
-            dp[i][j][LEN] = dp[i - 1][j - 1][LEN] + 1
-            dp[i][j][LCS] = dp[i - 1][j - 1][LCS] + A[j - 1]
+            dp[i][j] = dp[i - 1][j - 1] + A[j - 1]
         else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], key=lambda x: x[LEN])
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], key=lambda x: len(x))
 
-l, lcs = dp[len(B)][len(A)]
-print(l)
-print(lcs)
+print(len(dp[len(B)][len(A)]))
+print(dp[len(B)][len(A)])
