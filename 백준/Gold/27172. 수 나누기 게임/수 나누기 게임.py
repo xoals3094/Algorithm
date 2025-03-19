090129ae34492344228807c28179f867bbe4ade7
TaeMin Kim <100029499+xoals3094@users.noreply.github.com>
2025-03-19 10:43:50 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 232 ms, Memory: 138420 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -10,26 +10,13 @@ for n in numbers:
 
 score = [0 for _ in range(MAX + 1)]
 
-def calculate_factor(n):
-    _factor = []
-    i = 1
-    while i * i < n:
-        if n % i * i == 0:
-            _factor.append(i)
-            yield i
-        i += 1
-
-    if i * i == n:
-        yield i
-
-    for i in _factor:
-        yield n // i
-
 for n in numbers:
-    for factor in calculate_factor(n):
-        if flags[factor] and factor != n:
-            score[factor] += 1
-            score[n] -= 1
+    i = 2
+    while n * i <= MAX:
+        if flags[n * i]:
+            score[n] += 1
+            score[n * i] -= 1
+        i += 1
 
 for n in numbers:
     print(score[n], end=' ')
